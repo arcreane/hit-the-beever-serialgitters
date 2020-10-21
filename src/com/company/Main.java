e61f73022f992b6a9e55d91613748dceb49d0cdf
Mathieu Barbe-gayet <m.barbegayet@gmail.com>
2020-10-21 17:13:44 +0200

    Boucle de base pour quitter le jeu avec question

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean keepUp = false;
+        do {
+            //Appel des fonctions etc
+
+
+
+            //Demande si utilisateur veut continuer
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Recommencer?");
+            String reponse = sc.nextLine();
+            //Si la réponse est Y, on définit keepUp à true
+            if(reponse.equals("Y")){
+                keepUp=true;
+            }
+        }while(keepUp);
     }
 }
