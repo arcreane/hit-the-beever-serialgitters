52d06ca3ad5a66365b8284fbccf999562e0695de
jesusa <justinjhes@gmail.com>
2020-10-22 15:27:30 +0200

    separation des fichiers (menu et niveau de jeu)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,70 +5,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        boolean keepUp = false;
-        do {
-            //Appel des fonctions etc
-            Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-            //création de menu
-            System.out.println("Menu:");
-            System.out.println("1 : Nouvelle Partie");
-            System.out.println("2 : Résultats");
-            System.out.println("3 : Quitter");
+        //création de menu
+        System.out.println("Menu:");
+        System.out.println("1 : Nouvelle Partie");
+        System.out.println("2 : Résultats");
+        System.out.println("3 : Quitter");
 
-            String menu = input.nextLine();
-            switch (menu) {
-                case "1":
-                    System.out.println("menu : nouvellePartie");
-                    break;
-                case "2":
-                    System.out.println("menu : resultat");
-                    break;
-                case "3":
-                    System.out.println("menu : quitter");
-                    break;
-                default:
-                    System.out.println("vous avez pas mis le bon choix");
+        String menu = input.nextLine();
+        switch (menu) {
+            case "1":
+                System.out.println("menu : nouvellePartie");
+                NiveauDeJeu.nouvellePartie();
+                break;
+            case "2":
+                System.out.println("menu : resultat");
+                break;
+            case "3":
+                System.out.println("menu : quitter");
+                break;
+            default:
+                System.out.println("vous avez pas mis le bon choix");
+        }
 
-            }
-            System.out.println();
-
-
-            //creation de niveau
-            System.out.println("choissisez un niveau");
-            System.out.println("1 : facile : 5x5, 4 seconds");
-            System.out.println("2 : moyen : 7x7 , 3,5 seconds");
-            System.out.println("3 : difficile : 10x10, 3 second");
-
-            int difficulty;
-            String choixNiveau = input.nextLine();
-            switch (choixNiveau) {
-                case "1":
-                    difficulty=5;
-                    System.out.println("Niveau : facile");
-                    break;
-                case "2":
-                    difficulty=7;
-                    System.out.println("Niveau : moyen");
-                    break;
-                case "3":
-                    difficulty=10;
-                    System.out.println("Niveau : difficile");
-                    break;
-                default:
-                    System.out.println("Vous avez pas mis le bon choix");
-            }System.out.println();
-
-
-
-            //Demande si utilisateur veut continuer
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Recommencer?");
-            String reponse = sc.nextLine();
-            //Si la réponse est Y, on définit keepUp à true
-            if(reponse.equals("Y")){
-                keepUp=true;
-            }
-        }while(keepUp);
     }
 }
---
+++ src/com/company/NiveauDeJeu.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class NiveauDeJeu {
+
+    public static int nouvellePartie() {
+        Scanner input = new Scanner(System.in);
+
+        //creation de niveau
+        System.out.println("choissisez un niveau");
+        System.out.println("1 : facile : 5x5, 4 seconds");
+        System.out.println("2 : moyen : 7x7 , 3,5 seconds");
+        System.out.println("3 : difficile : 10x10, 3 second");
+
+
+        int difficulty=0;
+        String choixNiveau = input.nextLine();
+        if (choixNiveau.equals("1")) {
+            System.out.println("Niveau : facile");
+            difficulty = 5;
+        } else if (choixNiveau.equals("2"))  {
+            System.out.println("Niveau : moyen");
+            difficulty = 7;
+        } else if (choixNiveau.equals("3")) {
+            System.out.println("Niveau : difficile");
+            difficulty = 10;
+        } else{
+            System.out.println("Vous avez pas mis le bon choix");
+        }
+        return difficulty;
+
+    }
+
+}
+
